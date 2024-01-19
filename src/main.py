bd78f8e1de1e3d364b8eb89c02feef39f049a62f
rediahmdsubuntuwsl <redi.ngoding@gmail.com>
2024-01-19 15:16:27 +0700

    add histogram

--- src/main.py
+++ src/main.py
@@ -2,20 +2,21 @@ import psutil
 import time
 import os
 
-# from DOGZILLALib import DOGZILLA
-
-# g_dog = DOGZILLA()
-
 COUNTDOWN_TIME = 10
 
 def get_battery_percentage():
     battery = psutil.sensors_battery()
     percent = battery.percent
-    # batt = g_dog.read_battery()
     return round(percent)
-  
+
 def clean_terminal():
-		os.system('clear')
+    os.system('clear')
+
+def print_battery_histogram(percentage):
+    bar_length = 20
+    filled_length = int(bar_length * percentage / 100)
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    print(f'Battery: {percentage}% [{bar}]')
 
 if __name__ == "__main__":
     clean_terminal()
@@ -24,14 +25,15 @@ if __name__ == "__main__":
         battery_percentage = get_battery_percentage()
 
         while True:
-            print(f"\rBattery: {battery_percentage}% - Updating in {countdown_seconds}s", flush=True)
+            print_battery_histogram(battery_percentage)
+            print(f'Updating in {countdown_seconds}s', flush=True)
             time.sleep(1)
             countdown_seconds -= 1
             clean_terminal()
-            
+
             if countdown_seconds == 0:
                 countdown_seconds = COUNTDOWN_TIME
                 battery_percentage = get_battery_percentage()
-                
+
     except KeyboardInterrupt:
         print("\nExiting...")
